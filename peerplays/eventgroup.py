b2e65b798a86e6b6a711376f212d5c33dc97de74
Fabian Schuh <Fabian@chainsquad.com>
2017-05-31 12:43:55 +0200

    [api] add list_*() calls

--- peerplays/eventgroup.py
+++ peerplays/eventgroup.py
@@ -12,11 +12,20 @@ class EventGroup(dict):
     def __init__(
         self,
         identifier,
+        lazy=False,
         peerplays_instance=None,
     ):
-        self.identifier = identifier
         self.peerplays = peerplays_instance or shared_peerplays_instance()
-        self.refresh()
+        self.cached = False
+
+        if isinstance(identifier, str):
+            self.identifier = identifier
+            if not lazy:
+                self.refresh()
+        elif isinstance(identifier, dict):
+            self.cached = False
+            self.identifier = identifier.get("id")
+            super(EventGroup, self).__init__(identifier)
 
     def refresh(self):
         assert self.identifier[:5] == "1.18.",\
@@ -24,4 +33,38 @@ class EventGroup(dict):
         data = self.peerplays.rpc.get_object(self.identifier)
         if not data:
             raise EventGroupDoesNotExistException(self.identifier)
-        dict.__init__(data)
+        super(EventGroup, self).__init__(data)
+        self.cached = True
+
+    def __getitem__(self, key):
+        if not self.cached:
+            self.refresh()
+        return super(EventGroup, self).__getitem__(key)
+
+    def items(self):
+        if not self.cached:
+            self.refresh()
+        return super(EventGroup, self).items()
+
+    def __repr__(self):
+        return "<EventGroup %s>" % str(self.identifier)
+
+    @property
+    def sport(self):
+        from .sport import Sport
+        return Sport(self["sport_id"])
+
+
+class EventGroups(list):
+    """ List of all available EventGroups
+
+        :param str sport_id: Sport ID (``1.16.xxx``)
+    """
+    def __init__(self, sport_id, peerplays_instance=None):
+        self.peerplays = peerplays_instance or shared_peerplays_instance()
+        self.eventgroups = self.peerplays.rpc.list_event_groups(sport_id)
+
+        super(EventGroups, self).__init__([
+            EventGroup(x, lazy=True, peerplays_instance=peerplays_instance)
+            for x in self.eventgroups
+        ])
