ecae6a23515f3750dda9454b01282d45a6ce0fb5
Fabian Schuh <Fabian@chainsquad.com>
2017-09-09 16:01:57 +0200

    Added PPYTEST blockchain

--- peerplaysbase/chains.py
+++ peerplaysbase/chains.py
@@ -7,4 +7,8 @@ known_chains = {
         "chain_id": "594e284d3c733afaaa34a5e99a39edb31e5192fab023101d691d952034902237",
         "core_symbol": "PPY",
         "prefix": "PPY"},
+    "PPYTEST": {
+        "chain_id": "be6b79295e728406cbb7494bcb626e62ad278fa4018699cf8f75739f4c1a81fd",
+        "core_symbol": "PPYTEST",
+        "prefix": "PPYTEST"},
 }
