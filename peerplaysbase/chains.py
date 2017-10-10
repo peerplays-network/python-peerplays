b2cd15a5c92629e39b64a7a2503fc49ccb719da2
Fabian Schuh <Fabian@chainsquad.com>
2017-10-10 13:40:12 +0200

    update chainid

--- peerplaysbase/chains.py
+++ peerplaysbase/chains.py
@@ -1,6 +1,6 @@
 known_chains = {
     "PPY": {
-        "chain_id": "afbfd5bcaa0aa905a95b6a611b5dd553562cf26110d8b3e08d2a8b51a7af1438",
+        "chain_id": "6b6b5f0ce7a36d323768e534f3edb41c6d6332a541a95725b98e28d140850134",
         "core_symbol": "PPY",
         "prefix": "PPY"},
     "PPYT": {
