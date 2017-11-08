21e51e8aba1d85d17d3f76ee18bec6c2e143d5d1
Fabian Schuh <Fabian@chainsquad.com>
2017-11-08 12:08:22 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.1.3a'
+VERSION = '0.1.4'
 
 setup(
     name='peerplays',
