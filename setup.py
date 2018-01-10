755b40a73f6bc848172e1e1d2536c0a634a2556f
Fabian Schuh <Fabian@chainsquad.com>
2018-01-10 08:09:32 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 
 setup(
     name='peerplays',
@@ -46,7 +46,7 @@ setup(
         ],
     },
     install_requires=[
-        "graphenelib>=0.5.4",
+        "graphenelib>=0.5.6",
         "appdirs",
         "prettytable",
         "events==0.3",
