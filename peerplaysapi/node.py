3dd527906cc98fe15fe6d7330826ee0f96772a59
Fabian Schuh <Fabian@chainsquad.com>
2018-03-12 15:11:04 +0100

    Support HTTP(S) endpoints via HTTP/RPC

--- peerplaysapi/node.py
+++ peerplaysapi/node.py
@@ -1,5 +1,6 @@
 import re
 from grapheneapi.graphenewsrpc import GrapheneWebsocketRPC
+from grapheneapi.graphenehttprpc import GrapheneHTTPRPC
 from peerplaysbase.chains import known_chains
 from . import exceptions
 import logging
@@ -8,14 +9,28 @@ log = logging.getLogger(__name__)
 
 class PeerPlaysNodeRPC(GrapheneWebsocketRPC):
 
-    def __init__(self, *args, **kwargs):
-        super(PeerPlaysNodeRPC, self).__init__(*args, **kwargs)
+    def __init__(self,
+                 urls,
+                 user="",
+                 password="",
+                 **kwargs):
+        self._urls = urls
+        self.rpc.__init__(self, urls, **kwargs)
         self.chain_params = self.get_network()
 
-    def register_apis(self):
-        self.api_id["database"] = self.database(api_id=1)
-        self.api_id["history"] = self.history(api_id=1)
-        self.api_id["network_broadcast"] = self.network_broadcast(api_id=1)
+    @property
+    def rpc(self):
+        if isinstance(self._urls, (list, set)):
+            first_url = self._urls[0]
+        else:
+            first_url = self._urls
+
+        if first_url[:2] == "ws":
+            # Websocket connection
+            return GrapheneWebsocketRPC
+        else:
+            # RPC/HTTP connection
+            return GrapheneHTTPRPC
 
     def rpcexec(self, payload):
         """ Execute a call by sending the payload.
@@ -28,7 +43,7 @@ class PeerPlaysNodeRPC(GrapheneWebsocketRPC):
         """
         try:
             # Forward call to GrapheneWebsocketRPC and catch+evaluate errors
-            return super(PeerPlaysNodeRPC, self).rpcexec(payload)
+            return self.rpc.rpcexec(self, payload)
         except exceptions.RPCError as e:
             msg = exceptions.decodeRPCErrorMsg(e).strip()
             if msg == "missing required active authority":
@@ -79,3 +94,9 @@ class PeerPlaysNodeRPC(GrapheneWebsocketRPC):
             if v["chain_id"] == chain_id:
                 return v
         raise Exception("Connecting to unknown network!")
+
+    def __getattr__(self, name):
+        """ Map all methods to RPC calls and pass through the arguments.
+            It makes use of the GrapheneRPC library.
+        """
+        return self.rpc.__getattr__(self, name)
