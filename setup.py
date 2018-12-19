b00a95746e6472a40a11d5afd995985901fbe37a
Fabian Schuh <mail@xeroc.org>
2018-12-19 09:49:57 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,48 +5,40 @@ from setuptools import setup
 # Work around mbcs bug in distutils.
 # http://bugs.python.org/issue10945
 import codecs
-try:
-    codecs.lookup('mbcs')
-except LookupError:
-    ascii = codecs.lookup('ascii')
-    codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.3.0'
+try:
+    codecs.lookup("mbcs")
+except LookupError:
+    ascii = codecs.lookup("ascii")
+    codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
+
+VERSION = "0.3.1"
 
 setup(
-    name='peerplays',
+    name="peerplays",
     version=VERSION,
-    description='Python library for PEERPLAYS',
-    long_description=open('README.md').read(),
-    download_url='https://github.com/PBSA/python-peerplays/tarball/' + VERSION,
-    author='Fabian Schuh',
-    author_email='Fabian.Schuh@BlockchainProjectsBV.com',
-    maintainer='Fabian Schuh',
-    maintainer_email='Fabian.Schuh@BlockchainProjectsBV.com',
-    url='https://github.com/PBSA/peerplays-python',
-    keywords=['peerplays', 'library', 'api', 'rpc'],
-    packages=[
-        "peerplays",
-        "peerplays.cli",
-        "peerplaysapi",
-        "peerplaysbase",
-    ],
+    description="Python library for PEERPLAYS",
+    long_description=open("README.md").read(),
+    download_url="https://github.com/PBSA/python-peerplays/tarball/" + VERSION,
+    author="Fabian Schuh",
+    author_email="Fabian.Schuh@BlockchainProjectsBV.com",
+    maintainer="Fabian Schuh",
+    maintainer_email="Fabian.Schuh@BlockchainProjectsBV.com",
+    url="https://github.com/PBSA/peerplays-python",
+    keywords=["peerplays", "library", "api", "rpc"],
+    packages=["peerplays", "peerplays.cli", "peerplaysapi", "peerplaysbase"],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Topic :: Games/Entertainment',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Games/Entertainment",
     ],
-    entry_points={
-        'console_scripts': [
-            'peerplays = peerplays.cli.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["peerplays = peerplays.cli.cli:main"]},
     install_requires=open("requirements.txt").readlines(),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     include_package_data=True,
 )
