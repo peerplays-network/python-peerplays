b0d8e302caa0358fd56d674ad5c0567aac29f970
Fabian Schuh <mail@xeroc.org>
2018-12-21 10:04:31 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
     ascii = codecs.lookup("ascii")
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
 
-VERSION = "0.3.3"
+VERSION = "0.3.4"
 
 setup(
     name="peerplays",
