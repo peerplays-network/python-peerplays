bb1bda8a1415630110fcf93df4dc77ac73c8f191
Fabian Schuh <mail@xeroc.org>
2019-01-22 13:07:17 +0100

    Remove message from memo

--- peerplays/asset.py
+++ peerplays/asset.py
@@ -1,6 +1,5 @@
 import json
 from peerplaysbase.asset_permissions import todict
-from .blockchainobject import BlockchainObject
 from .exceptions import AssetDoesNotExistsException
 from .instance import BlockchainInstance
 
