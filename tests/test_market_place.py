41c959d4f429d5bbe28721df0bd57da1171457d9
Jemshid KK <jemshidkk@gmail.com>
2022-05-18 09:31:55 +0000

    CI/CD and test cases working successfully

--- tests/test_market_place.py
+++ tests/test_market_place.py
@@ -25,15 +25,53 @@ class Testcases(unittest.TestCase):
 
         self.setUp()
 
-        offers = peerplays.rpc.get_offers_by_item("1.29.0", "1.31.5", 1) 
+        self.res = peerplays.nft_metadata_create("1.2.9",
+                                                 self.nameMetadata,
+                                                 self.nameMetadata,
+                                                 self.nameMetadata,
+                                                 revenue_partner="1.2.8",
+                                                 revenue_split=300,
+                                                 is_sellable=False,
+                                                 is_transferable=False)
+        # peerplays.blocking = False
+        print("nft_metadata_create Success!")
+
+        self.metadataId = self.res["operation_results"][0][1]
+        print("metadataId:", self.metadataId)
+
+        peerplays.nft_metadata_update("1.2.9",
+                                      self.metadataId,
+                                      self.nameMetadata + "m",
+                                      self.nameMetadata + "m",
+                                      self.nameMetadata + "m",
+                                      "1.2.9",
+                                      400,
+                                      True,
+                                      True)
+        print("nft_metadata_update Success!")
+
+        self.res = peerplays.nft_mint("1.2.9",
+                           self.metadataId,
+                           "1.2.9",
+                           "1.2.9",
+                           "1.2.9",
+                           self.nameNft)
+        print("nft_mint Success!")
+
+        self.tokenId = self.res["operation_results"][0][1]
+
+        # offers = peerplays.rpc.get_offers_by_item("1.29.0", "1.31.5", 1) 
+        offers = peerplays.rpc.get_offers_by_item("1.29.0", self.tokenId, 1) 
         if len(offers) >= 1:
             offer = offers[0]
             peerplays.cancel_offer("1.2.9", offer["id"]) 
 
-        peerplays.create_offer(["1.31.5"], "1.2.9", {"amount":5,"asset_id":"1.3.0"}, {"amount":15,"asset_id":"1.3.0"}, False, "2030-09-18T11:05:39", "") 
+        # peerplays.create_offer(["1.31.5"], "1.2.9", {"amount":5,"asset_id":"1.3.0"}, {"amount":15,"asset_id":"1.3.0"}, False, "2030-09-18T11:05:39", "") 
+        peerplays.create_offer([self.tokenId], "1.2.9", {"amount":5,"asset_id":"1.3.0"}, {"amount":15,"asset_id":"1.3.0"}, False, "2030-09-18T11:05:39", "") 
         print("create_off Success!")
 
-        offers = peerplays.rpc.get_offers_by_item("1.29.0", "1.31.5", 1) 
+        # offers = peerplays.rpc.get_offers_by_item("1.29.0", "1.31.5", 1) 
+        offers = peerplays.rpc.get_offers_by_item("1.29.0", self.tokenId, 1) 
         offer = offers[0]
         peerplays.create_bid("1.2.10", {"amount":8,"asset_id":"1.3.0"}, offer["id"]) 
         print("create_bid Success!")
