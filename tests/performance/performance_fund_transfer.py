a18bcece8fb2645308f8a48a54eef7f45711f2b2
Jemshid KK <jemshidkk@gmail.com>
2022-10-24 17:38:06 +0530

    wip

--- tests/performance/performance_fund_transfer.py
+++ tests/performance/performance_fund_transfer.py
@@ -2,4 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import multiprocessing
+import time
+import random
+
+
+def task():
+    print("   ")
+    rand = random.randint(1000,2000)
+    print(rand, "Started")
+    time.sleep(5)
+    print(rand, "Done")
+    print("------------------------------")
+    print ("   ") 
 
