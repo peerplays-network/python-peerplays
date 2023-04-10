5632e5562805ba5add1fad7ebafc1cde2bd0f2fe
Christopher Sanborn <csanborn@peerplays.com>
2023-04-10 13:44:28 -0400

    Bump version to 0.5.1 in preparation for release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except LookupError:
     ascii = codecs.lookup("ascii")
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
 
-VERSION = "0.6.0-dev"
+VERSION = "0.5.1"
 
 # Strip some stuff from README for PyPI description:
 long_desc = open("README.md").read()
